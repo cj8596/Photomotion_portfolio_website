c4210244739adfc0a1e2c69312f39380ef2152a4
Cristin Johnson <cristinjohnson96@gmail.com>
2025-07-25 19:54:51 +0530

    cors

--- ContactUs/contact_script.js
+++ ContactUs/contact_script.js
@@ -16,22 +16,19 @@ function initializeFormSubmission() {
   form.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    const data = {
-      name: form.name.value,
-      brand: form.bname.value,
-      phone: form.phone.value,
-      email: form.email.value,
-      product: form.product.value,
-      service: form.service.value,
-      message: form.message ? form.message.value : "" 
-    };
+    const formData = new URLSearchParams();
+    formData.append("name", form.name.value);
+    formData.append("brand", form.bname.value);
+    formData.append("phone", form.phone.value);
+    formData.append("email", form.email.value);
+    formData.append("product", form.product.value);
+    formData.append("service", form.service.value);
+    formData.append("message", form.message?.value || "");
 
     fetch("https://script.google.com/macros/s/AKfycbzn1ALRNer9VdAL-FcSm0lhpz7ofqtIkyCoRcUrQ-B5D4r_0jLV4aV1FdiN902MZeHhbw/exec", {
       method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json"
-      }
+      body: formData
+      // No headers! Let the browser set Content-Type to x-www-form-urlencoded
     })
       .then((res) => res.json())
       .then((res) => {
