7b0cc195a8e90f21814b910876ba93968dc2f17a
Cristin Johnson <cristinjohnson96@gmail.com>
2025-10-20 23:02:18 +0530

    changes - media query

---
+++ images/js/jewellery_images.js
@@ -0,0 +1,14 @@
+const portfolioImages = [
+  {
+    main: "https://picsum.photos/id/1005/1200/900",
+    sides: ["https://picsum.photos/id/1011/800/1000", "https://picsum.photos/id/1012/800/1000"]
+  },
+  {
+    main: "https://picsum.photos/id/1006/1200/900",
+    sides: ["https://picsum.photos/id/1015/800/1000"]
+  },
+  {
+    main: "https://picsum.photos/id/1008/1200/900",
+    sides: []
+  }
+];
