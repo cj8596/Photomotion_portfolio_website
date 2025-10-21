38f39bc58c5ba458947833a37c0afa54d0de710e
Cristin Johnson <cristinjohnson96@gmail.com>
2025-10-21 13:17:32 +0530

    fixes

--- images/js/food_images.js
+++ images/js/food_images.js
@@ -1,14 +1,82 @@
 const portfolioImages = [
-  { id: "1-2KB2oGLGkFXhaUEJW8x6Nx4tzKQ4Gms", title: "Fashion 1" },
-  { id: "1B7zO-zNM_2rYalmT4aOX5S1hHqzh0wVk", title: "Fashion 2" },
-  { id: "1BMgSIBk9cbtnQH10X2DNvb-XGlDWO0Cn", title: "Fashion 3" },
-  { id: "1DDQE6JoTgBMbOSMjS8_pCr9uYXQLkQUi", title: "Fashion 4" },
-  { id: "1G5jjhb95Fya-nCd2M153Jdh7fjokZZPO", title: "Fashion 5" },
-  { id: "1QijAW7GCZLXQ1F7_q-yFUPYYZs78QI7L", title: "Fashion 6" },
-  { id: "1TVB_etIzxME1cHI3AMeuW-h_ORsXafdS", title: "Fashion 7" },
-  { id: "1hQ-9b3dW5mrs46YgLfgpN_8m9aAneAzd", title: "Fashion 8" },
-  { id: "1kFhPfvt6H8_DCpd_OFntDPZiMLwCFwf-", title: "Fashion 9" },
-  { id: "1kQoKwv7MBY6BRz2rTtUHR-GjOyIQBYbq", title: "Fashion 10" },
-  { id: "1omqvSlD8oh5_oaIL7bIqD2ueHZs0bdRk", title: "Fashion 11" },
-  { id: "1ww8NxxhU-KGb2cIa2JS2yg6jV6dr7lOp", title: "Fashion 12" },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/Biscoff+Shake++Insta+post+copy.webp",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/Biscoff+Shake+Close+Up++Insta+post+copy.webp",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/Blueberry+cheese+pastry++Closeup+post+size+copy.webp",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/Blueberry+cheese+pastry++post+size+copy.webp",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/Donuts+insta+post+copy.webp",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/Ferrero+Roscher+Pastry+Cutting+copy.webp",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/IMG_1129+copy+2.webp",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/IMG_1204+copy+2.webp",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/IMG_1262+copy+2.webp",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/IMG_1297+copy+2.webp",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/IMG_1400+copy+2.webp",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/IMG_1478+copy+2.webp",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/IMG_1478+copy+3.webp",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/IMG_1493+copy+2.webp",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/Kiwi+ElderFlower+post+size+copy.webp",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/Mandarin+Mojito+Post+size+copy.webp",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/Passion+Fruit+With+Boba+post+size+copy.webp",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/Screenshot+2025-06-16+162506.jpg",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/blueberry+close+up+shot+insta+post+copy.webp",
+    "sides": []
+  },
+  {
+    "main": "https://www.photomotion-mp.com/Media/food/donuts+02+post+size+copy.webp",
+    "sides": []
+  }
 ];
